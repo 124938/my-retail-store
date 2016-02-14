9aa291bf511b62ff5331a221ee524d548092eb58
124938 <shrey.9129@gmail.com>
2016-02-14 23:35:49 +0530

    Updated JUnit test case for DefaultCartService

--- myretailstore-parent/myretailstore-service/src/test/java/com/mediaocean/myretailstore/cart/impl/DefaultCartServiceTest.java
+++ myretailstore-parent/myretailstore-service/src/test/java/com/mediaocean/myretailstore/cart/impl/DefaultCartServiceTest.java
@@ -7,6 +7,7 @@ import java.util.Date;
 import java.util.List;
 
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.apache.cxf.endpoint.Server;
 import org.apache.cxf.interceptor.LoggingInInterceptor;
@@ -25,6 +26,7 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.jaxrs.json.JacksonJsonProvider;
 import com.mediaocean.myretailstore.cart.CartDto;
+import com.mediaocean.myretailstore.cart.CartItemDto;
 import com.mediaocean.myretailstore.cart.CartService;
 import com.mediaocean.myretailstore.domain.common.Product;
 import com.mediaocean.myretailstore.domain.common.ProductCategory;
@@ -32,6 +34,7 @@ import com.mediaocean.myretailstore.domain.common.ProductRepository;
 import com.mediaocean.myretailstore.domain.order.OrderRepository;
 import com.mediaocean.myretailstore.domain.order.OrderStatus;
 import com.mediaocean.myretailstore.domain.order.OrderSummary;
+import com.mediaocean.myretailstore.domain.order.dal.impl.RetailOrderDao;
 import com.mediaocean.myretailstore.domain.order.impl.RetailOrder;
 import com.mediaocean.myretailstore.domain.order.impl.RetailOrderItem;
 
@@ -41,6 +44,7 @@ public class DefaultCartServiceTest {
 	
 	private OrderRepository orderRepositoryMock;
 	private ProductRepository productRepositoryMock;
+	private RetailOrderDao retailOrderDaoMock;
 	
 	private Server server;
 
@@ -48,6 +52,7 @@ public class DefaultCartServiceTest {
 	public void initialize() throws Exception {
 		orderRepositoryMock = EasyMock.createMock(OrderRepository.class);
 		productRepositoryMock = EasyMock.createMock(ProductRepository.class);
+		retailOrderDaoMock = EasyMock.createMock(RetailOrderDao.class);
 
 		// Create resource provider
 		SingletonResourceProvider resourceProvider = new SingletonResourceProvider(new DefaultCartService(orderRepositoryMock, productRepositoryMock), true);
@@ -121,6 +126,38 @@ public class DefaultCartServiceTest {
 		EasyMock.verify(orderRepositoryMock);
 	}
 	
+	@Test
+	public void updateCartItemTest() {
+		long orderId = System.currentTimeMillis();
+
+		RetailOrder mockOrder = prepareMockRetailOrder(orderId);
+		EasyMock.expect(orderRepositoryMock.fetchOrder(orderId)).andReturn(mockOrder);
+		EasyMock.replay(orderRepositoryMock);
+
+		String productCode = "PRD-007";
+		EasyMock.expect(productRepositoryMock.fetchProductDetails(productCode)).andReturn(prepareMockProduct(productCode, "test", "test", ProductCategory.B, 100d));
+		EasyMock.replay(productRepositoryMock);
+		
+		retailOrderDaoMock.modify(mockOrder);
+		EasyMock.expectLastCall();
+		EasyMock.replay(retailOrderDaoMock);
+		
+		WebClient client = prepareWebClient();
+		client.type(MediaType.APPLICATION_JSON_TYPE);
+		client.path(orderId + "/update");
+		
+		CartItemDto cartItem = new CartItemDto();
+		cartItem.setCode("PRD-007");
+		cartItem.setQuantity(10);
+		
+		Response response = client.post(cartItem);
+		Assert.assertNotNull(response);
+		
+		EasyMock.verify(orderRepositoryMock);
+		EasyMock.verify(productRepositoryMock);
+		EasyMock.verify(retailOrderDaoMock);
+	}
+	
 	private OrderSummary prepareMockOrderSummary(long orderId, Date orderDate, OrderStatus orderStatus) {
 		OrderSummary os = new OrderSummary(orderId);
 		os.setOrderDate(orderDate);
@@ -149,7 +186,7 @@ public class DefaultCartServiceTest {
 	}
 	
 	private RetailOrder prepareMockRetailOrder(long orderId) {
-		RetailOrder order = new RetailOrder(null);
+		RetailOrder order = new RetailOrder(retailOrderDaoMock);
 		order.setOrderSummary(prepareMockOrderSummary(orderId, new Date(), OrderStatus.CREATED));
 		order.addRetailOrderItem(prepareMockRetailOrderItem("JUnit - 007", "JUnit - 007 - Name", "JUnit - 007 - desc", ProductCategory.A, 200d, 10));
 		order.addRetailOrderItem(prepareMockRetailOrderItem("JUnit - 008", "JUnit - 008 - Name", "JUnit - 008 - desc", ProductCategory.B, 100d, 20));
